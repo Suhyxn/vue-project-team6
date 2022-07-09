590a2184e8e45c47795c2fc6847049a16546da23
SongE12 <aassdd6434@naver.com>
2022-07-09 15:41:06 +0900

    merge test

--- src/store/admin.js
+++ src/store/admin.js
@@ -1,32 +1,32 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 
-export const useAdminStore = defineStore("admin", {
+export const useAdminStore = defineStore('admin', {
   state() {
     return {
       Product: [],
-    };
+    }
   },
   getters: {},
   actions: {
     hello() {
-      console.log("hello");
+      console.log('hello')
     },
     async ReadProductAdmin() {
-      console.log("hello");
+      console.log('hello')
       const res = await fetch(
-        "https://asia-northeast3-heropy-api.cloudfunctions.net/api/products ",
+        'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products ',
         {
-          method: "GET",
+          method: 'GET',
           headers: {
-            "content-type": "application/json",
-            apikey: "FcKdtJs202204",
-            username: "Team6",
-            masterKey: "true",
+            'content-type': 'application/json',
+            apikey: 'FcKdtJs202204',
+            username: 'Team6',
+            masterKey: 'true',
           },
         }
-      );
-      const master = await res.json();
-      console.log(master);
+      )
+      const master = await res.json()
+      console.log(master)
     },
   },
-});
+})
