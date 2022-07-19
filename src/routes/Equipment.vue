bb836eceaec4d1869823a14ebd0bb95c7527647a
SeungHun6450 <toffg6450@naver.com>
2022-07-19 23:04:40 +0900

    merge and add  netlify history mode

--- src/routes/Equipment.vue
+++ src/routes/Equipment.vue
@@ -54,7 +54,7 @@ export default {
     handler(payload){
         if(!sessionStorage.getItem('token')){
           alert('제품구매는 로그인을 해야 할 수 있습니다!')
-          this.$router.push('/signin')
+          this.$router.push('/sign/signin')
           return 
         }
       this.clientStore.singleProductData = payload
